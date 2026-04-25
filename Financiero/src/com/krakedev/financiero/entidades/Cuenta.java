b56a1503a53221d0b98b3b17c2cda05789a4703c
Coug4r <burneofrancisco2005@gmail.com>
2026-04-25 12:04:21 -0500

    Add: atributo cliente con geters y seters en las clase Cuenta, Update: metodo imprimir para el nuevo atributo

--- Financiero/src/com/krakedev/financiero/entidades/Cuenta.java
+++ Financiero/src/com/krakedev/financiero/entidades/Cuenta.java
@@ -4,6 +4,7 @@ public class Cuenta {
 	private String id;
 	private double SaldoActual;
 	private String tipo;
+	private Cliente cliente;
 	
 	public Cuenta(String id) {
 		super();
@@ -11,6 +12,14 @@ public class Cuenta {
 		this.SaldoActual = 0;
 		this.tipo = "A";
 		
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 
 	public String getId() {
@@ -33,10 +42,12 @@ public class Cuenta {
 	}
 	
 	public void imprimir() {
+		String msgCliente = "Cedula: "+cliente.getCedula()
+				+"\nPropietario: "+cliente.getNombre() + " "+cliente.getApellido();
 		String msgCuenta = "ID: "+ id
 				+ "\nSaldo Actual: "+ SaldoActual
 				+"\nTipo: "+ tipo;
-		System.out.println(msgCuenta);
+		System.out.println(msgCliente + "\n" + msgCuenta);
 	}
 	
 }
