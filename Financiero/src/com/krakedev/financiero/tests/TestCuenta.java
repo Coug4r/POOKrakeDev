131aeee9b0b3ae0eebd1a4bf629062822ee9d662
Coug4r <burneofrancisco2005@gmail.com>
2026-04-25 12:07:36 -0500

    Add: TestCuenta en el nuevo Paquete tests prueba de nullPoiter

---
+++ Financiero/src/com/krakedev/financiero/tests/TestCuenta.java
@@ -0,0 +1,15 @@
+package com.krakedev.financiero.tests;
+
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class TestCuenta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cuenta cuentaTest = new Cuenta("001");
+		
+		cuentaTest.imprimir();
+
+	}
+
+}
