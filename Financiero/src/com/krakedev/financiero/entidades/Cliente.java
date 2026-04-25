0c283e7e2df68cb36dc066382b35fdcadf631fa0
Coug4r <burneofrancisco2005@gmail.com>
2026-04-25 12:00:20 -0500

    Create: Clase Cliente con constructor

---
+++ Financiero/src/com/krakedev/financiero/entidades/Cliente.java
@@ -0,0 +1,13 @@
+package com.krakedev.financiero.entidades;
+
+public class Cliente {
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	public Cliente(String cedula, String nombre, String apellido) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+}
