230fc374cd9d569abb98422df2b8339f4d629a92
Coug4r <burneofrancisco2005@gmail.com>
2026-04-25 12:00:57 -0500

    Add: geters y seters clase Cliente

--- Financiero/src/com/krakedev/financiero/entidades/Cliente.java
+++ Financiero/src/com/krakedev/financiero/entidades/Cliente.java
@@ -10,4 +10,22 @@ public class Cliente {
 		this.nombre = nombre;
 		this.apellido = apellido;
 	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 }
